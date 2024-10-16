60b83d21652676c4d7df3d7c61b63868c22a9d51
Elias-48 <esru20062025@gmail.com>
2024-10-15 22:49:45 -0500

    Cambios hechos por Elias ( cambio 1)

---
+++ TareaSemana8/src/FUNCIONARÁ/FUNCIONARÁ_no_se_sabe_xD.java
@@ -0,0 +1,10 @@
+
+package FUNCIONARÁ;
+
+public class FUNCIONARÁ_no_se_sabe_xD {
+
+    public static void main(String[] args) {
+        System.out.println("Si puedes ver este mensaje es que god si funciona waaaaaaa te amo y feliz x los 5 meses");
+    }
+
+}
